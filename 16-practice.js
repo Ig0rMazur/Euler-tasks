b54d7be1f752c3597617d1055b2f16e927ed3e88
S0metinge <123563860+Ig0rMazur@users.noreply.github.com>
2023-03-23 16:46:45 +0300

    Create 16-practice.js

---
+++ 16-practice.js
@@ -0,0 +1,6 @@
+function sum(number){
+  return number.toString().split('').reduce((acc, item) => acc += +item, 0);
+} 
+console.time('TIME')
+console.log(sum(BigInt(2**1000)));
+console.timeEnd('TIME');
