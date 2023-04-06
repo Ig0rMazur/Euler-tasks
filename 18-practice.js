fafed873516d8d782f18bf7b72021f151ef2e3cf
S0metinge <123563860+Ig0rMazur@users.noreply.github.com>
2023-04-06 16:43:03 +0300

    Update 18-practice.js

--- 18-practice.js
+++ 18-practice.js
@@ -1,29 +1,45 @@
 const mainArray =
   [
-    [75],
-    [95,64],
-    [17,47,82],
-    [18,35,87,10],
-    [20,4,82,47,65],
-    [19,1,23,75,3,34],
-    [88,2,77,73,7,63,67],
-    [99,65,4,28,6,16,70,92],
-    [41,41,26,56,83,40,80,70,33],
-    [41,48,72,33,47,32,37,16,94,29],
-    [53,71,44,65,25,43,91,52,97,51,14],
-    [70,11,33,28,77,73,17,78,39,68,17,57],
-    [91,71,52,38,17,14,91,43,58,50,27,29,48],
+                         [75],
+                       [95,64],
+                      [17,47,82],
+                    [18,35,87,10],
+                   [20,4,82,47,65],
+                  [19,1,23,75,3,34],
+                 [88,2,77,73,7,63,67],
+                [99,65,4,28,6,16,70,92],
+              [41,41,26,56,83,40,80,70,33],
+            [41,48,72,33,47,32,37,16,94,29],
+          [53,71,44,65,25,43,91,52,97,51,14],
+        [70,11,33,28,77,73,17,78,39,68,17,57],
+      [91,71,52,38,17,14,91,43,58,50,27,29,48],
     [63,66,4,68,89,53,67,30,73,16,69,87,40,31],
-    [4,62,98,27,23,9,70,98,73,93,38,53,60,4,23],
+   [4,62,98,27,23,9,70,98,73,93,38,53,60,4,23],
 ]
-
+//great max 1313
 function maxSum(arr){
-  sum = 0;
-  arr.forEach((element) => {
-    sum += Math.max(...element)
-  });
-  return sum
+  arr = arr.map((mainItem, mainIndex) => { // mainItem - подмассив 
+    if( mainIndex === 0 ) return mainItem
+    else {
+      let newArr = mainItem.map((item, index, array) => { // item - el подмассива
+        if( (index !== 0) && (index !== mainItem.length-1)){
+          maxItem = arr[mainIndex-1][index-1] && (arr[mainIndex-1][index-1] > arr[mainIndex-1][index]) ? arr[mainIndex-1][index-1] : arr[mainIndex-1][index] 
+        }
+          if(index === 0){
+            maxItem = arr[mainIndex-1][index] 
+          } 
+          if(index === mainItem.length-1){
+            maxItem = arr[mainIndex-1][index-1]
+          }
+          array[index] = item + maxItem
+          return item += maxItem 
+        })
+        return newArr
+    }
+  })
+  console.log(Math.max(...arr[14]))
 }
 console.time('maxSum')
 maxSum(mainArray)
 console.timeEnd('maxSum')
+
